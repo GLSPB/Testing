17b69960265e6cb98612687f310fe83ac603a2a0
Лебедев Григорий Александрович <1lga@spbt.local>
2022-12-17 11:06:21 +0300

    Исправлены ошибки

--- volume/cube.cpp
+++ volume/cube.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 
 using namespace std;
-
+int i;
 void cheers(int n){
-for (integer i = 0; i < n; i)
-cou << "Cheers! "
-cout << endl
+for ( i == 0; i < n; i++)
+cout << "Cheers! ";
+cout << endl;
 }
 
-double cube(double x)
-{
+double cube(double x) {
 return x * x * x;
 }
 
-int main(){
-cheеrs(5); // вызов функции
+int main() {
+double side;
 cout << "Give me a number: ";
-double side
-cin >> side
-double volume = cube(syde) // вызов функции
-cout « "A " « side «"-foot cube has a volume of ";
-cout « volume << " cubic feet.\n";
-cheers (cub(2))
-retyrn 0;
+cin >> side;
+double volume = cube(side); // вызов функции
+cout << "A " << side << "-foot cube has a volume of ";
+cout << volume << " cubic feet.\n";
+cheers (cube(3));
+return 0;
 }
