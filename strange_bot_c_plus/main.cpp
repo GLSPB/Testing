dc3fc8c575282e4b81f2eac1f329cc5da0131df8
Лебедев Григорий Александрович <1lga@spbt.local>
2022-12-17 11:05:08 +0300

    Исправлены ошибки

--- strange_bot_c_plus/main.cpp
+++ strange_bot_c_plus/main.cpp
@@ -1,6 +1,6 @@
-include <iostream>
-include <string>
-
+#include <iostream>
+#include <string>
+// был пропущен служебный знак #
 using namespace std;
 
 /**
@@ -8,25 +8,32 @@ using namespace std;
  * @brief main
  * @return
  */
-int main
-{
-    string botName := "Your Brains";
+main() { 
+ // main это функция, а не переменная
+    string botName = "Your Brains";
+// прописан лишний знак ":"
     string userName, dayOfWeek;
 
-    сout << "Hello. I am " << botName << endl
-    сout << "What is your name?" << endl
-    getline(cin; userName)
-    cout << "Hello " << userName << endl
-    cout << "What is weekday now?" << endl
-    getline(cin; dayOfWeek);
-    if(dayOfWeek = "monday") {
+    cout << "Hello. I am " << botName << endl;
+    cout << "What is your name?" << endl;
+	// в строках 17,18 пропущенн служебный хнак ";"
+    getline(cin, userName);
+	// не правельный знак ";"
+    cout << "Hello " << userName << endl;
+    cout << "What is weekday now?" << endl;
+	// в строках 22,23 пропущенн служебный хнак ";"
+    getline(cin, dayOfWeek);
+    if (dayOfWeek == "monday") {
+	//недописан знак "="
         cout << "Sunday is so far away!\n";
     }
-    elseif(dayOfWeek = "friday") {
-        cout < "Hurraaa!\n";
+    else if (dayOfWeek == "friday") {
+	//недописан знак "="
+        cout << "Hurraaa!\n";
     }
     else {
-        cout < "Working day ... Hrr\n";
+        cout << "Working day ... Hrr\n";
     }
-    retyrn 0;
+    return 0;
+	//допущенна орфографическая ошибка в слове "return"
 }
